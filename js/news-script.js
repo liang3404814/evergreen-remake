bf8a20904eea6ccd23ce9f5ee75b5adc332611f2
Willie Taing <taingw4@uw.edu>
2013-12-04 19:09:52 -0800

    added link to news page from index, added link to index from news page. News entries are now dynamically generated. In the future we can add a news event simply by adding one to the top of the news.js. It doens't look the best right now, but it's functional.

--- js/news-script.js
+++ js/news-script.js
@@ -3,5 +3,18 @@ $(function() {
 });
 
 function renderNews() {
-	
+	var idx;
+	var entry;
+	var instance;
+	var template = $('.news-template');
+	for (idx = 0; idx < news.entries.length; idx++) {
+		console.log(news.entries[idx]);
+		entry = news.entries[idx];
+		instance = template.clone();
+		instance.find('.date').html(entry.date);
+		instance.find('.title').html(entry.title);
+		instance.find('.content').html(entry.content);
+		instance.removeClass('news-template');
+		$('.news-entries').append(instance);
+	}
 }
--- js/news.js
+++ js/news.js
@@ -1,7 +1,8 @@
 var news = {
 	entries: [
-		{date: 'December 4, 2013', title: 'Launch of new website!', content: 'Today is the launch of our new website! Please take a look around!'},
+		{date: 'April 4, 2014', title: 'Willie\'s 21st birthday!', content: 'Today is Willie\'s 21st birthday! Make sure to say happy birthday to him! Captain AMERICA!'},
 		{date: 'January 1, 2013', title: 'Happy New Years!', content: 'Happy New Years everybody! Please enjoy! But be safe! Don\'t drink and drive!'},		
-		{date: 'April 4, 2014', title: 'Willie\'s 21st birthday!', content: 'Today is Willie\'s 21st birthday! Make sure to say happy birthday to him! Captain AMERICA!'}
+		{date: 'December 4, 2013', title: 'Launch of new website!', content: 'Today is the launch of our new website! Please take a look around!'}
+		
 	]
 };
