282116aaefac64a03452385fd759fd4a0f92a81e
liang3404814 <liang3404814@gmail.com>
2013-12-05 13:42:41 -0800

    reassembled pages using AngularJS

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var myApp = /**
+* egc Module
+*
+* Overarching module for the EGC website
+*/
+angular.module('egc', [ 'ui.router', 'ngRoute']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+
+	// redirects default landing and non-matched requests to "/" aka home
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+
+	.state('home', {
+		url: "/",
+		templateUrl: "partials/home.html"
+	})
+	.state('about', {
+		url: "/about",
+		templateUrl: "partials/about.html"
+	})
+	.state('news', {
+		url: "/news",
+		templateUrl: "partials/news.html"
+	})
+	.state('repertoire', {
+		url: "/repertoire",
+		templateUrl: "partials/repertoire.html"
+	})
+	.state('events', {
+		url: "/events",
+		templateUrl: "partials/events.html"
+	})
+	.state('gallery', {
+		url: "/gallery",
+		templateUrl: "partials/gallery.html"
+	})
+	.state('contact', {
+		url: "/contact",
+		templateUrl: "partials/contact.html"
+	})
+});
