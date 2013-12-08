464820c937c95ae1c23e20aefb43182005d00c82
liang3404814 <liang3404814@gmail.com>
2013-12-08 13:23:45 -0800

    removed mediaelement from repertoire

--- js/script.js
+++ js/script.js
@@ -44,16 +44,16 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 });
 
 // Setting up mediaelement player directive
-myApp.directive('mediaelement', function() {
-	return {
-		restrict: 'A',
-		link: function(scope, element, attrs) {
-			attrs.$observe('src', function() {
-				element.mediaelementplayer();
-			});
-		}
-	}
-});
+// myApp.directive('mediaelement', function() {
+// 	return {
+// 		restrict: 'A',
+// 		link: function(scope, element, attrs) {
+// 			attrs.$observe('src', function() {
+// 				element.mediaelementplayer();
+// 			});
+// 		}
+// 	}
+// });
 
 
 
